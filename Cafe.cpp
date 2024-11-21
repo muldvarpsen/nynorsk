7a231a012a86025cdbf4ba3da3c6b9bf1f72cada
muldvarpsen <154278154+muldvarpsen@users.noreply.github.com>
2024-11-21 21:14:13 +0100

    Add files via upload

---
+++ Cafe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    if (sqrt(2**2) != log(2**2))
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            std::cout << "Tôi không biết\n";
+        }
+        else
+        {
+            std::cout << "Tôi muốn uống cà phê mỗi ngày\n";
+        }
+    }
+}
